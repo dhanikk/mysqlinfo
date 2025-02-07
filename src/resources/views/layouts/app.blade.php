6a2a50deab3a251ca981c7935b74f2e6db39a80c
Vidhi Thakkar <vidhit.itpath@gmail.com>
2025-02-07 10:20:29 +0530

    version changed

--- src/resources/views/layouts/app.blade.php
+++ src/resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="description" content="This is an PHP and MySQL server information tool">
+    <meta name="description" content="This is a MySQL server information tool">
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
